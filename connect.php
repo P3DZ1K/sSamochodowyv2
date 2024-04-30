05b4a60fbc602914061a2b59ce7037b7bb2c71a5
P3DZ1K <dawidpedziwiatr@gmail.com>
2024-04-30 10:21:36 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,6 +4,6 @@
 $host = "localhost"; // adres serwera MySQL
 $db_user = "root"; // nazwa użytkownika MySQL
 $db_password = ""; // hasło użytkownika MySQL
-$db_name = "ssamochodowy"; // nazwa bazy danych MySQLdwqdwqdwq
+$db_name = "ssamochodowy"; // nazwa bazy danych
 
 ?>
